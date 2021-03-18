0529c4e1a140c2793a31d8d58c05b3084626e674
MarioDuval <77450981+MarioDuval@users.noreply.github.com>
2021-03-18 19:57:13 +0100

    App

--- src/cat/mvm/gui/entities/MyFrame.java
+++ src/cat/mvm/gui/entities/MyFrame.java
@@ -5,13 +5,16 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class MyFrame extends JFrame{
-    private JButton jbtAceptar;
+    private JButton jbtOK;
     private JLabel jlbFirstName;
     private JLabel jlbLastName;
     private JLabel jlbPosition;
     private JTextField jtfFirstName;
     private JTextField jtfLastName;
     private JTextField jtfPosition;
+    private JTextArea jtaData;
+    private JButton jbtClear;
+    private Object objTextField;
 
     public MyFrame(){
         /*Toolkit screen = Toolkit.getDefaultToolkit();
@@ -40,7 +43,9 @@ public class MyFrame extends JFrame{
         jtfFirstName = new JTextField();
         jtfLastName = new JTextField();
         jtfPosition = new JTextField();
-        jbtAceptar = new JButton();
+        jbtOK = new JButton();
+        jtaData = new JTextArea();
+        jbtClear = new JButton();
 
         getContentPane().setLayout(null);
 
@@ -48,8 +53,8 @@ public class MyFrame extends JFrame{
         getContentPane().add(jlbFirstName);
         jlbFirstName.setBounds(12, 28, 116, 14);
 
-        jtfFirstName.setHorizontalAlignment(JTextField.RIGHT);
-        /*jtfFirstName.addFocusListener(new FocusAdapter() {
+        jtfFirstName.setHorizontalAlignment(JTextField.LEFT);
+        jtfFirstName.addFocusListener(new FocusAdapter() {
             @Override
             public void focusGained(FocusEvent e) {
                 jtfFirstNamefocusGained(e);
@@ -58,10 +63,79 @@ public class MyFrame extends JFrame{
         jtfFirstName.addKeyListener(new KeyAdapter() {
             @Override
             public void keyTyped(KeyEvent e) {
-                jtfFirstNamekeyTyped(e);
+                jtfFirstNameKeyTyped(e);
             }
-        });*/
+        });
         getContentPane().add(jtfFirstName);
-        jtfFirstName.setBounds(120, 28, 350, 20 );
+        jtfFirstName.setBounds(80, 25, 350, 20 );
+
+        jlbLastName.setText("Last Name");
+        getContentPane().add(jlbLastName);
+        jlbLastName.setBounds(12, 68, 104, 24);
+
+        jtfLastName.setHorizontalAlignment(JTextField.LEFT);
+        jtfLastName.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                jtfLastNameFocusGained(e);
+            }
+        });
+        jtfLastName.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                jtfLastNameKeyTyped(e);
+            }
+        });
+        getContentPane().add(jtfLastName);
+        jtfLastName.setBounds(80, 70, 350, 20);
+
+        jlbPosition.setText("Position");
+        getContentPane().add(jlbPosition);
+        jlbPosition.setBounds(12, 108, 104, 24);
+
+        jtfPosition.setHorizontalAlignment(JTextField.LEFT);
+        jtfPosition.addFocusListener(new FocusAdapter() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                jtfPositionFocusGained(e);
+            }
+        });
+        jtfPosition.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                jtfPositionKeyTyped(e);
+            }
+        });
+        getContentPane().add(jtfPosition);
+        jtfPosition.setBounds(80, 112, 350, 20);
+
+        jbtOK.setText("OK");
+        jbtOK.setMnemonic('O');
+        getRootPane().setDefaultButton(jbtOK);
+        getContentPane().add(jbtOK);
+        jbtOK.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jbtOKActionPerformed(e);
+            }
+        });
+        jbtOK.setBounds(355, 160, 75, 24);
+
+
+        getContentPane().add(jtaData);
+        jtaData.setBounds(12, 200,550, 200);
+
+        jbtClear.setText("Clear");
+        jbtClear.setMnemonic('C');
+        getRootPane().setDefaultButton(jbtClear);
+        getContentPane().add(jbtClear);
+        jbtClear.setBounds(487, 410, 75,24);
+
+    }
+
+    private void jtfDataKeyTyped(KeyEvent evt) { objTextField = evt.getSource(); }
+
+    private void jtfDataFocusGained(FocusEvent evt) {
+        JTextField objEnfocado = (JTextField) evt.get
     }
 }
